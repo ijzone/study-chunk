d8783672c8715fd47007dc4b6af6f59cf7727738
Ikjoo Lee <ijzone.dev@gmail.com>
2024-08-06 17:00:45 +0900

    Add minimax sum problem

---
+++ TIL/algorithm/hankerRank/warmUp/miniMaxSum/MiniMaxSum.java
@@ -0,0 +1,44 @@
+package com.ij.study.warmUp.miniMaxSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class MiniMaxSum {
+
+    static void miniMaxSum(List<Integer> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < arr.size() - i - 1; j++) {
+                if (arr.get(j) > arr.get(j + 1)) {
+                    int temp = arr.get(j);
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, temp);
+                }
+            }
+        }
+        System.out.println("arr = " + arr);
+        long min = 0;
+        long max = 0;
+        for (int i = 0; i < arr.size()-1; i++) {
+            min += arr.get(i);
+            max += arr.get(arr.size() - i - 1);
+        }
+        System.out.println(min + " " + max);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
