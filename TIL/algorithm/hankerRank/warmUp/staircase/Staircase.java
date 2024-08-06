0e6b8f0e44c7b4d407be6844a8b2b8bcf409770b
Ikjoo Lee <ijzone.dev@gmail.com>
2024-08-06 16:19:57 +0900

    Add staircase problem

---
+++ TIL/algorithm/hankerRank/warmUp/staircase/Staircase.java
@@ -0,0 +1,34 @@
+package com.ij.study.warmUp.staircase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Staircase {
+
+    static void staircase(int n) {
+        int spaceCount = n - 1;
+        int stairCount = n - spaceCount;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < spaceCount; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < stairCount; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+            spaceCount--;
+            stairCount++;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        staircase(n);
+
+        bufferedReader.close();
+    }
+}
